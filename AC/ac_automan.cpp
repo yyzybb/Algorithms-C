f771c9365bea53e4118b9504227a03dd9e0a8f9e
yuyangzi <yuyangzi@meizu.com>
2015-06-18 12:50:02 +0800

    AC自动机: 支持replace, 支持utf8字符替换

--- AC/ac_automan.cpp
+++ AC/ac_automan.cpp
@@ -1,160 +1,21 @@
 #include <iostream>
-#include <vector>
-#include <queue>
+#include "ac_automan.h"
+#include <string.h>
 
-class AC_Automan
-{
-    static const int char_size = 256;
-
-    private:
-        struct Node
-        {
-            bool finally;
-            int deep;
-            Node* fail;
-            Node* next[char_size];
-            bool ref[char_size];
-
-            Node() : finally(false), deep(0), fail(NULL), next{}, ref{} {}
-        };
-
-        Node root_;
-        Node* state_;
-
-    public:
-        AC_Automan() : state_(&root_) {
-            root_.fail = &root_;
-        }
-
-        ~AC_Automan() {
-            Clear();
-        }
-
-        void Clear() {
-            std::queue<Node*> Q;
-            for (int i = 0; i < char_size; ++i) {
-                Node *child = root_.next[i];
-                root_.next[i] = NULL;
-                if (root_.ref[i]) continue;
-                if (!child) continue;
-                Q.push(child);
-            }
-
-            while (!Q.empty()) {
-                Node* nd = Q.front();
-                Q.pop();
-
-                for (int i = 0; i < char_size; ++i) {
-                    Node *child = nd->next[i];
-                    if (!child) continue;
-
-                    if (!nd->ref[i]) {
-                        Q.push(child);
-                    }
-                }
-
-                delete nd;
-            }
-        }
-
-        void AddPattern(std::string const& p) {
-            if (p.empty()) return ;
-
-            Node* pos = &root_;
-            for (std::size_t i = 0; i < p.size(); ++i) {
-                unsigned char next_index = p[i];
-                if (!pos->next[next_index]) {
-                    pos->next[next_index] = new Node{};
-                    pos->next[next_index]->deep = pos->deep + 1;
-                }
-
-                pos = pos->next[next_index];
-            }
-            pos->finally = true;
-        }
-
-        void CreateFailPointer() {
-            std::queue<Node*> Q;
-            for (int i = 0; i < char_size; ++i) {
-                if (root_.next[i]) {
-                    Q.push(root_.next[i]);
-                    root_.next[i]->fail = &root_;
-                }
-            }
-
-            while (!Q.empty()) {
-                Node* nd = Q.front();
-                Q.pop();
-
-                for (int i = 0; i < char_size; ++i) {
-                    Node *child = nd->next[i];
-                    if (child) {
-                        Q.push(child);
-                        child->fail = Next(nd->fail, (char)i);
-                    }
-                }
-            }
-        }
-
-        void CreateMatrix() {
-            std::queue<Node*> Q;
-            Q.push(&root_);
-
-            while (!Q.empty()) {
-                Node* nd = Q.front();
-                Q.pop();
-
-                for (int i = 0; i < char_size; ++i) {
-                    Node *child = nd->next[i];
-                    if (child) {
-                        Q.push(child);
-                    } else {
-                        nd->next[i] = Next(nd->fail, (char)i);
-                        nd->ref[i] = true;
-                    }
-                }
-            }
-        }
-
-        void Reset() {
-            state_ = &root_;
-        }
-
-        void solve(std::string const& source, void(*cb)(int, int, std::string const&))
-        {
-            Reset();
-            for (int i = 0; i < source.size(); ++i) {
-                state_ = Next(state_, source[i]);
-                if (state_->finally) {
-                    cb(i - state_->deep + 1, state_->deep, source);
-
-                    // sub string.
-                    Node* pos = state_->fail;
-                    for (; pos->finally; pos = pos->fail) {
-                        cb(i - pos->deep + 1, pos->deep, source);
-                    }
-                }
-            }
-        }
-
-    private:
-        Node* Next(Node* state, char value) {
-            unsigned char i = value;
-            if (state->next[i]) {
-                return state->next[i];
-            } else if (state == &root_) {
-                return &root_;
-            }
-
-            return Next(state->fail, value);
-        }
-};
-
-void dump(int pos, int n, std::string const& source) {
-    std::cout << "match [" << pos << "]. keyword: [" << source.substr(pos, n) << "]" << std::endl;
+void dump(int pos, std::string const& pattern) {
+    std::cout << "match [" << pos << "]. keyword: [" << pattern << "]" << "size:" << pattern.size() << std::endl;
 }
 
-int main()
+std::string replace(std::string const& pattern) {
+    int c = utf8_char_c(pattern);
+    std::string result;
+    result.resize(c);
+    memset(&result[0], '*', c);
+    //std::cout << "replace c=" << c << ", result=" << result << std::endl;
+    return result;
+}
+
+void test1()
 {
     const char* patterns[] = {
         "she",
@@ -170,8 +31,38 @@ int main()
         ac.AddPattern(patterns[i]);
     ac.CreateFailPointer();
     ac.CreateMatrix();
-    ac.solve(source, dump);
-    ac.Clear();
+    ac.Match(source, dump);
+
+    // replace
+    std::string r = ac.Replace(source, replace);
+    std::cout << "replace result:" << r << std::endl;
+}
+
+void test2()
+{
+    const char* patterns[] = {
+        "的",
+        "朋友",
+        "好朋友"
+    };
+
+    std::string source = "我们的好是朋友";
+    AC_Automan ac;
+    for (int i = 0; i < sizeof(patterns) / sizeof(const char*); ++i)
+        ac.AddPattern(patterns[i]);
+    ac.CreateFailPointer();
+    ac.CreateMatrix();
+    ac.Match(source, dump);
+
+    // replace
+    std::string r = ac.Replace(source, replace);
+    std::cout << "replace result:" << r << std::endl;
+}
+
+int main()
+{
+    test1();
+    test2();
     return 0;
 }
 
---
+++ AC/ac_automan.h
@@ -0,0 +1,191 @@
+#include <vector>
+#include <queue>
+#include <string>
+
+class AC_Automan
+{
+    static const int char_size = 256;
+
+    private:
+        struct Node
+        {
+            bool finally;
+            int deep;
+            Node* fail;
+            Node* next[char_size];
+            bool ref[char_size];
+
+            Node() : finally(false), deep(0), fail(NULL), next{}, ref{} {}
+        };
+
+        Node root_;
+        Node* state_;
+
+    public:
+        AC_Automan() : state_(&root_) {
+            root_.fail = &root_;
+        }
+
+        ~AC_Automan() {
+            Clear();
+        }
+
+        void Clear() {
+            std::queue<Node*> Q;
+            for (int i = 0; i < char_size; ++i) {
+                Node *child = root_.next[i];
+                root_.next[i] = NULL;
+                if (root_.ref[i]) continue;
+                if (!child) continue;
+                Q.push(child);
+            }
+
+            while (!Q.empty()) {
+                Node* nd = Q.front();
+                Q.pop();
+
+                for (int i = 0; i < char_size; ++i) {
+                    Node *child = nd->next[i];
+                    if (!child) continue;
+
+                    if (!nd->ref[i]) {
+                        Q.push(child);
+                    }
+                }
+
+                delete nd;
+            }
+        }
+
+        void AddPattern(std::string const& p) {
+            if (p.empty()) return ;
+
+            Node* pos = &root_;
+            for (std::size_t i = 0; i < p.size(); ++i) {
+                unsigned char next_index = p[i];
+                if (!pos->next[next_index]) {
+                    pos->next[next_index] = new Node{};
+                    pos->next[next_index]->deep = pos->deep + 1;
+                }
+
+                pos = pos->next[next_index];
+            }
+            pos->finally = true;
+        }
+
+        void CreateFailPointer() {
+            std::queue<Node*> Q;
+            for (int i = 0; i < char_size; ++i) {
+                if (root_.next[i]) {
+                    Q.push(root_.next[i]);
+                    root_.next[i]->fail = &root_;
+                }
+            }
+
+            while (!Q.empty()) {
+                Node* nd = Q.front();
+                Q.pop();
+
+                for (int i = 0; i < char_size; ++i) {
+                    Node *child = nd->next[i];
+                    if (child) {
+                        Q.push(child);
+                        child->fail = Next(nd->fail, (char)i);
+                    }
+                }
+            }
+        }
+
+        void CreateMatrix() {
+            std::queue<Node*> Q;
+            Q.push(&root_);
+
+            while (!Q.empty()) {
+                Node* nd = Q.front();
+                Q.pop();
+
+                for (int i = 0; i < char_size; ++i) {
+                    Node *child = nd->next[i];
+                    if (child) {
+                        Q.push(child);
+                    } else {
+                        nd->next[i] = Next(nd->fail, (char)i);
+                        nd->ref[i] = true;
+                    }
+                }
+            }
+        }
+
+        void Reset() {
+            state_ = &root_;
+        }
+
+        void Match(std::string const& source, void(*cb)(int, std::string const&))
+        {
+            Reset();
+            for (std::size_t i = 0; i < source.size(); ++i) {
+                state_ = Next(state_, source[i]);
+                if (state_->finally) {
+                    cb(i - state_->deep + 1, source.substr(i - state_->deep + 1, state_->deep));
+
+                    // sub string.
+                    Node* pos = state_->fail;
+                    for (; pos->finally; pos = pos->fail) {
+                        cb(i - pos->deep + 1, source.substr(i - pos->deep + 1, pos->deep));
+                    }
+                }
+            }
+        }
+
+        std::string Replace(std::string const& source, std::string(*replace)(std::string const&))
+        {
+            Reset();
+            int offset = 0;
+            std::string result(source);
+            for (std::size_t i = 0; i < source.size(); ++i) {
+                state_ = Next(state_, source[i]);
+                if (state_->finally) {
+                    std::string pattern = source.substr(i - state_->deep + 1, state_->deep);
+                    std::string dst = replace(pattern);
+                    result.replace(i - state_->deep + 1 + offset, pattern.size(), dst);
+                    offset += (int)dst.size() - (int)pattern.size();
+                    Reset();
+                }
+            }
+            return result;
+        }
+
+    private:
+        Node* Next(Node* state, char value) {
+            unsigned char i = value;
+            if (state->next[i]) {
+                return state->next[i];
+            } else if (state == &root_) {
+                return &root_;
+            }
+
+            return Next(state->fail, value);
+        }
+};
+
+static int utf8_char_c(std::string const& str)
+{
+    int c = 0;
+    std::size_t i = 0;
+    while (i < str.size()) {
+        char ch = str[i];
+        int c_len = 1;
+        while (ch & 0x80) {
+            ch = ch << 1;
+            ++c_len;
+        }
+
+        if (c_len > 6)
+            c_len = 1;
+
+        ++c;
+        i += c_len;
+    }
+    return c;
+}
+
